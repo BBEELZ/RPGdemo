2c98909e368db532a4daaca219922a6d145a5888
Ben Beeler <benjamin_beeler@bellsouth.net>
2024-06-13 15:45:54 -0400

    Updated Character Class

---
+++ Source/RPGDemo/RpgPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RpgPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGDEMO_API ARpgPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
